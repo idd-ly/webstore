3fcb727ed79bd520d82198a1704099c2fb68595f
Lyndon Joseph <lyndon@Lyndons-MacBook-Pro.local>
2019-12-11 12:47:59 -0500

    lol

--- js/index.js
+++ js/index.js
@@ -10,32 +10,32 @@
 const products = [
   {
     name: `Tyler the Creator - Igor`,
-    price: `35.09`,
+    price: `$35.09`,
     img: `img/tylerevolution.jpg`,
     description: `Tyler, the Creator’s sixth album is impressionistic and emotionally charged, the result of an auteur refining his style and baring more of his soul than ever before.`,
     qty: `IN STOCK`,
-    idty: `Product 012`,
+    idty: `Product: 012`,
   }, {
   name: `Frank Ocean - Moon River`,
-  price: `50.00`,
+  price: `$50.00`,
   img: `img/moonriver.jpg`,
   description: `Frank Ocean's cover of Andy William's original composition "Moon River" delivers a new twist of vocals harmonies within the current era of music.`,
   qty: `OUT OF STOCK`,
-  idty:  `Product 013`
+  idty:  `Product: 013`
   },
 
 {
   name:`Run The Jewels -RTJ II`,
-  price: `41.86`,
+  price: `$41.86`,
   img:`img/rtj.jpg`,
   description: `Run the Jewels, also refered to as RTJ is a dynamic duo of rapper and producer. From their current debut of the original, self-titled Run the Jewels, is still considered a classic today. With the sequel, it follows up its previous scores and improves on it.`,
   qty:  `IN STOCK`,
-  idty: `Product 014`
+  idty: `Product: 014`
 },
 
 {
   name:`BROCKHAMPTON - SATURATION II`,
-  price: `37.99`,
+  price: `$37.99`,
   img:`img/sat.jpg`,
   description: `BROCKHAMPTON, the self proclaimed best boyband in the world drop their latest album, Saturation II. `,
   qty:  `OUT OF STOCK`,
@@ -44,7 +44,7 @@ const products = [
 
 {
   name:`Playboy Carti - Die Lit`,
-  price: `29.99`,
+  price: `$29.99`,
   img:`img/dielit.jpg`,
   description: `Atlanta rapper, Playboy Carti's debut full-length studio ablbum, creates new rap conventions for the new age of hip-hop.`,
   qty:  `IN STOCK`,
@@ -53,7 +53,7 @@ const products = [
 
 {
   name:`Freddie Gibbs & Madlib - Bandana`,
-  price: `36.99`,
+  price: `$36.99`,
   img:`img/fre.jpg`,
   description: `One of the most consistent records this year, by two of Hip-hop's contrasting figures.`,
   qty:  `OUT OF STOCK`,
@@ -62,7 +62,7 @@ const products = [
 
 {
   name:`E-Dubble - Plan A`,
-  price: `23.99`,
+  price: `$23.99`,
   img:`img/pna.jpg`,
   description: `A stylistic tone that is necessary for E-Dubble's music, with his most recent album drop.`,
   qty:  `OUT OF STOCK`,
@@ -71,7 +71,7 @@ const products = [
 
 {
   name:`Frank Ocean - Chanel Orange`,
-  price: `16.99`,
+  price: `$16.99`,
   img:`img/cho.jpg`,
   description: `After making a splash with his first mixtape, Frank Ocean is killing it in the world of contemporary R&B with this new album..`,
   qty:  `OUT OF STOCK`,
@@ -80,7 +80,7 @@ const products = [
 
 {
   name:`Childish Gambino - Because the Internet`,
-  price: `15.95`,
+  price: `$15.95`,
   img:`img/bti.jpg`,
   description: ` Because The Internet, the most ambitious album from multi-talented figure, Childish Gambino.  `,
   qty:  `IN STOCK`,
@@ -89,7 +89,7 @@ const products = [
 
 {
   name:`Vince Staples - FM!`,
-  price: `100.99`,
+  price: `$100.99`,
   img:`img/fml.jpeg`,
   description: `FM!'s fun concept begs for a longer runtime, but even at 22 minutes the project has plenty to offer with its quality beats, flows, hooks, and lyrics.`,
   qty:  `OUT OF STOCK`,
@@ -98,7 +98,7 @@ const products = [
 
 {
   name:`The Weeknd - Kiss Land`,
-  price: `47.99`,
+  price: `$47.99`,
   img:`img/ksl.png`,
   description: `The Canadian experimental R&B project the Weekend goes from his mixtape, to his first commerical studio album.`,
   qty:  `IN STOCK`,
@@ -116,7 +116,7 @@ const products = [
 
 {
   name:`Janelle Monáe - Dirty Computer`,
-  price: `44.99`,
+  price: `$44.99`,
   img:`img/dct.png`,
   description: `Dirty Computer is Janelle Monáe's poppiest album to date, as well as her most deeply sexual and political.`,
   qty:  `OUT OF STOCK`,
@@ -125,7 +125,7 @@ const products = [
 
 {
   name:`KIDS SEE GHOSTS -Self-Titled DELUXE EDITION `,
-  price: `249.99`,
+  price: `$249.99`,
   img:`img/ksg.jpg`,
   description: `KIDS SEE GHOSTS finds both Kanye West and Kid Cudi in top form as they host a psychedelic odyssey through the darkest and brightest corners of their psyches.`,
   qty:  `OUT OF STOCK`,
@@ -134,7 +134,7 @@ const products = [
 
 {
   name:`Kendrick Lamar - To Pimp a Butterfly`,
-  price: `34.99`,
+  price: `$34.99`,
   img:`img/tpb.png`,
   description: `Several years ago, Kendrick Lamar was hip hop's underdog. Today, he's dropping what's possibly the best rap album of the decade.`,
   qty:  `IN STOCK`,
@@ -143,7 +143,7 @@ const products = [
 
 {
   name:`Jaden Smith - Syre`,
-  price: `34.99`,
+  price: `$34.99`,
   img:`img/syr.jpg`,
   description: `Jaden Smith's latest film is an auditory one, exploring the lyrical follies of in desperate need of perspective.`,
   qty:  `OUT OF STOCK`,
@@ -152,7 +152,7 @@ const products = [
 
 {
   name:`MadeinTYO - Thank You Mr. Tokyo`,
-  price: `89.99`,
+  price: `$89.99`,
   img:`img/tyt.jpg`,
   description: `After making the undeniably great "Uber Everywhere", Madeintyo comes back with his most recent release.`,
   qty:  ` IN STOCK `,
@@ -161,7 +161,7 @@ const products = [
 
 {
   name:`Anderson Paak - Oxnard`,
-  price: `32.99`,
+  price: `$32.99`,
   img:`img/oxd.jpg`,
   description: `Oxnard has a stronger sense of direction than its predecessor and is a breath of fresh air in the current state of pop rap and neo-soul.`,
   qty:  `IN STOCK`,
